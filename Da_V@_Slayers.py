c8137dee316305554ba051c0e6e30c4ea146ad52
mart_laptop <mart.veldkamp@gmail.com>
2022-10-29 17:42:17 +0200

    Updated capsule code, deleted get/update env function

--- Da_V@_Slayers.py
+++ Da_V@_Slayers.py
@@ -26,9 +26,9 @@ def createTeam(firstIndex, secondIndex, isRed,
 
 class ChonkyBoy(CaptureAgent):
 
+  # NOTE: Dit wordt als init 1x gerunned. Voor ons belangrijk voor mogelijk:
   def registerInitialState(self, gameState):
     """
-      NOTE: Dit wordt als init 1x gerunned. Voor ons belangrijk voor mogelijk:
         - Default parameters init
           - alpha:    learning rate
           - epsilon:  exploration rate
@@ -44,14 +44,12 @@ class ChonkyBoy(CaptureAgent):
     self.observationHistory = []
     self.QValue = util.Counter()
     
-    # f = open('./Da_V@_Slayers.py', 'r')
-    # content = f.read()
-    # print(content)
-    # f.close()
     # self.env = self.FriendlyFood(gameState)
     # print(self.env)
-    self.walls_temp = self.WallsNormalization(gameState)
-    print(self.walls_temp)
+    # self.caps = self.FriendlyCapsules(gameState)
+    # print(self.caps)
+    # self.walls_temp = self.WallsNormalization(gameState)
+    # print(self.walls_temp)
     
     CaptureAgent.registerInitialState(self, gameState)
 
@@ -81,7 +79,6 @@ class ChonkyBoy(CaptureAgent):
     
     return random.choice(Possible_Actions)
     
-
   def FriendlyFood(self, gameState):
     if gameState.isOnRedTeam(self.index):
       Friendly_food = gameState.getRedFood()
@@ -150,38 +147,38 @@ class ChonkyBoy(CaptureAgent):
 
     return Food_coordinates_norm
   
-    
   def FriendlyCapsules(self, gameState):
     # [a, Q(s,a) for a in s.getlegalactions]
   
     if gameState.isOnRedTeam(self.index):
-      Friendly_capsules = gameState.getRedCapsules()
+      friendly_caps = gameState.getRedCapsules()
     else:
-      Friendly_capsules = gameState.getBlueCapsules()
+      friendly_caps = gameState.getBlueCapsules()
     
-    Pac_Pos = gameState.getAgentPosition(self.index)
-    Pac_Pos = list(Pac_Pos)
-    Normal_Friendly_capsules = [0,0]
-    Normal_Friendly_capsules[0] = Pac_Pos[0] - Friendly_capsules[0][0]
-    Normal_Friendly_capsules[1] = Pac_Pos[1] - Friendly_capsules[0][1]
+    pac_pos = gameState.getAgentPosition(self.index)
+    
+    normal_friendly_caps = []
+    
+    for coordinate in friendly_caps:
+      normal_friendly_caps.append(tuple(map(lambda i, j: i - j, coordinate, pac_pos)))
 
-    return Normal_Friendly_capsules
+    return normal_friendly_caps
 
   def EnemyCapsules(self, gameState):
     # [a, Q(s,a) for a in s.getlegalactions]
   
     if gameState.isOnRedTeam(self.index):
-      Friendly_capsules = gameState.getBlueCapsules()
+      enemy_caps = gameState.getBlueCapsules()
     else:
-      Friendly_capsules = gameState.getRedCapsules()
+      enemy_caps = gameState.getRedCapsules()
     
-    Pac_Pos = gameState.getAgentPosition(self.index)
-    Pac_Pos = list(Pac_Pos)
-    Normal_Friendly_capsules = [0,0]
-    Normal_Friendly_capsules[0] = Pac_Pos[0] - Friendly_capsules[0][0]
-    Normal_Friendly_capsules[1] = Pac_Pos[1] - Friendly_capsules[0][1]
-    print(Normal_Friendly_capsules)
-    return Normal_Friendly_capsules
+    pac_pos = gameState.getAgentPosition(self.index)
+    normal_enemy_caps = []
+    
+    for coordinate in enemy_caps:
+      normal_enemy_caps.append(tuple(map(lambda i, j: i - j, coordinate, pac_pos)))
+    
+    return normal_enemy_caps
       
   def WallsNormalization(self, gameState):
     walls = gameState.getWalls()
@@ -213,92 +210,3 @@ class ChonkyBoy(CaptureAgent):
       wall_coordinates_norm.append(tuple(map(lambda i, j: i - j, coordinate, pacman_position)))
 
     return wall_coordinates_norm
-  
-  def getEnvironment(self, gameState):
-    """
-      Returned 2D-numpy array (datatype: str) met alle belangrijke init environment info:
-        - Walls         = 'W'
-        - Food          = 'FF, EF'
-        - powercapsule  = 'FP, EP'
-        - Empty space   = '_'
-        - Agent         = 'A, FA'
-    """
-    
-    # Krijg de locatie van de walls en stops ze in de array
-    env = gameState.getWalls()
-    cop_env = []
-    for r in env:       # Voor elke row in env
-      cop_env.append(r)
-    np_env = np.array(cop_env).astype(str)
-    
-    # Intereer over de np array en verrander de values
-    np_env[np_env == "True"] = "W"
-    np_env[np_env == "False"] = "_"
-    
-
-    # Krijg de locatie van de power capsules, en voeg toe aan np_env
-    Is_red = gameState.isOnRedTeam(self.index)
-    R_caps = gameState.getRedCapsules()
-    B_caps = gameState.getBlueCapsules()
-    
-    for r in R_caps:
-      if(Is_red):
-        np_env[r[0]][r[1]] = "FP"
-      else:
-        np_env[r[0]][r[1]] = "EP"
-    
-    for r in B_caps:
-      if(Is_red == 0):
-        np_env[r[0]][r[1]] = "FP"
-      else:
-        np_env[r[0]][r[1]] = "EP"
-    
-    # Krijg de locatie van alle food
-    R_food = gameState.getRedFood()
-    B_food = gameState.getBlueFood()
-    cop_env_R_food = []
-    cop_env_B_food = []
-    
-    
-    for r in R_food:
-      cop_env_R_food.append(r)
-    np_env_R_food = np.array(cop_env_R_food).astype(str)
-
-    for r in B_food:
-      cop_env_B_food.append(r)
-    np_env_B_food = np.array(cop_env_B_food).astype(str)
-    
-    # Intereer over de np array en verrander de values
-    solutions1 = np.argwhere(np_env_R_food == "True")
-    for p in solutions1:
-      if(Is_red):
-        np_env[p[0]][p[1]] = "1"
-      else:
-        np_env[p[0]][p[1]] = "0"
-        
-    solutions2 = np.argwhere(np_env_B_food == "True")
-    for p in solutions2:
-      if(Is_red == 0):
-        np_env[p[0]][p[1]] = "1"
-      else:
-        np_env[p[0]][p[1]] = "0"
-
-    
-    # Krijg positie van Agent
-    pos = gameState.getAgentPosition(self.index)
-    np_env[pos[0]][pos[1]] = "A"
-    
-    # Je kan deze print aanzetten voor debugging / check hoe de env er uit ziet.
-    print(np_env)
-    
-    return np_env
-  
-  def UpdateEnvironment(self, gameState, np_env):
-    # Updates agent data in the env
-    np_env[np_env == "A"] = "_"
-    
-    pos = gameState.getAgentPosition(self.index)
-    np_env[pos[0]][pos[1]] = "A"
-    
-    return np_env
-  
